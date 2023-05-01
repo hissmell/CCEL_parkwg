1e158a158c166ab9832f52dd0a01ca3ea2503008
unknown <pn50212@hanyang.ac.kr>
2023-05-01 09:19:34 +0900

    .

--- park_lib/run_poscar.py
+++ park_lib/run_poscar.py
@@ -15,7 +15,7 @@ poscar_path = args.poscar_path
 working_dir = args.working_dir
 move_dir(working_dir)
 
-RESTART = check_restart(args.restart)
+RESTART = check_restart(args.restart,working_dir=working_dir)
 ''' write calculation setting! '''
 magmom = {"Pt" : 2, "Fe" : 4, "Co" : 3, "Ni" : 2, "Cu" : 1, "Zn" : 0}
 
