15bea77a28bc006919e4dd38940973803a5f46df
hissmell <pn50212@hanyang.ac.kr>
2023-07-04 13:05:54 +0900

    asdf

--- park_lib/run_poscar.py
+++ park_lib/run_poscar.py
@@ -18,16 +18,35 @@ potcar = args.potcar
 move_dir(working_dir)
 
 RESTART = check_restart(args.restart_false,working_dir=working_dir)
+
 ''' write calculation setting! '''
-magmom = {"Pt" : 2, "Fe" : 4, "Co" : 3, "Ni" : 2, "Cu" : 1, "Zn" : 0}
-
 atoms = read(poscar_path)
-host_atom_num = len(atoms[[atom.symbol == atoms.symbols[0] for atom in atoms]])
-rest_atom_num = len(atoms) - host_atom_num
 
-init_magmom_list = [magmom[atoms.symbols[0]]] * host_atom_num + [0.0] * rest_atom_num
+# set initial magmom
+magmom = {'Sc':'1','Ti':'2','V':'3','Cr':'6'
+        ,'Mn':'5','Fe':'4','Co':'3','Ni':'2'
+        ,'Cu':'1','Zn':'0','Y':'1','Zr':'2'
+        ,'Nb':'5','Mo':'6','Tc':'5','Ru':'4'
+        ,'Rh':'3','Pd':'0','Ag':'1','Cd':'0'
+        ,'La':'1','Hf':'2','Ta':'3','W':'4'
+        ,'Re':'5','Os':'4','Ir':'3','Pt':'2'
+        ,'Au':'1'}
+
+atoms_symbol_list = [atom.symbol for atom in atoms]
+init_magmom_list = [0.0] * len(atoms)
+for atom_idx in range(len(atoms)):
+    try:
+        m = float(magmom[atoms_symbol_list[atom_idx]])
+    except:
+        m = 0.0
+    init_magmom_list[atom_idx] = m
+
 atoms.set_initial_magnetic_moments(init_magmom_list)
 
+# set detailed potcar setting if you want
+setups = {"base" : potcar, "W" : "_sv"} # {"Atom symbol" : "suffix" | "Atom integer index" : "Atom symbol + suffix"}
+
+# set incar setting
 calc = Vasp(
         directory=working_dir,
         kpts=(3, 3, 1),
@@ -51,11 +70,6 @@ calc = Vasp(
         restart=RESTART
     )
 
+# do calculation!
 _ = atoms.get_potential_energy()
-
-
-
-
-
-
 
